a41c8d4e367b388bec4d3e82969dc4febb01c22a
GeorgeRabinovich <georgyb2000@gmail.com>
2024-02-16 01:40:03 +0300

    new file:   .streamlit/config.toml 	modified:   README.md 	modified:   car_app.py 	new file:   image.jpg 	new file:   notebook/EDA.ipynb 	new file:   notebook/vehicles_us.csv 	new file:   requirements.txt 	new file:   vehicles_us.csv

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # car_project
 My car sales project 
 
-The app script is in the car_app.py file
+This is the project about exploring data of used cars. 
+
+There are a few histograms and selectboxes that will help you to get more information about used cars dataset.
+
+To implement this project have been used libraries such as pandas, streamlit, plotly and pyplot and streamlit methods such as st.write, st.title, st.checkbox, st.plotly chart and so on.
+
+To launch this project on your local machine you should open the car_app.py file and write in the terminal  python3 -m streamlit run .\car_app.py    
+
+
+
+The directory has:
+README.md file that has explanation about project 
+vehicles_us.csv file that is car advertisment dataset
+notebook folder that contains EDA.ipynb (Jupiter notebook) file that used for exploring and preprocessing data, the code and important marks about the code. 
+requirements.txt file with required packages
+.streamlit directory that contains config.toml file with streamlit configurations 
--- car_app.py
+++ car_app.py
@@ -1,1 +1,71 @@
+"""Used car explorer app"""
 
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')  #reading the csv file
+
+columns_to_replace = ['model_year', 'cylinders', 'odometer', 'paint_color', 'is_4wd']
+
+for column in columns_to_replace:
+    df[column] = df[column].fillna('unknown')
+
+st.title('Used cars properties')
+st.subheader('Explore the properties of a used cars!')
+
+st.image('image.jpg')
+
+st.header('Get familiar with the used cars data:')
+
+st.dataframe(df)
+
+df['manufacturer'] = df['model'].str.extract(r'(\b\w+)') #getting the column with manufacturer
+
+st.header('Vehicle models by year and condition:')
+
+best = st.checkbox('The best condition cars')
+
+if best: #if else for checkbox
+    df=df[df.condition == 'new']
+else:
+    df=df
+
+fig = px.scatter(df, x="model_year", y="condition", color="model") #condition scatterplot
+st.plotly_chart(fig)
+
+data_price = st.header('Check the average cars price by manufacturer:')
+
+avg_price = px.histogram(df, x='manufacturer',
+                         y="price", histfunc='avg',
+                         color='manufacturer') #avg price histogram
+st.plotly_chart(avg_price)
+
+select = st.header('Celect the car by your preferences:')
+
+fuel_select = st.selectbox(
+   "Select the fuel type:",
+   df['fuel'].apply(lambda x: x.split()[0]).unique(),
+   index=None,
+   placeholder="Fuel type",
+)
+
+transmission_select = st.selectbox(
+   "Select the transmission type:",
+   df['transmission'].apply(lambda x: x.split()[0]).unique(),
+   index=None,
+   placeholder="Transmission type",
+)
+
+type_select = st.selectbox(
+   "Select the car type:",
+   df['type'].apply(lambda x: x.split()[0]).unique(),
+   index=None,
+   placeholder="Car type",
+)
+
+prefs = df[(df['fuel'] == fuel_select) &
+           (df['transmission'] == transmission_select) &
+           (df['type'] == type_select)]
+
+st.dataframe(prefs)
